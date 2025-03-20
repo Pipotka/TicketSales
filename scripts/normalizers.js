9aa37e6d66dd2989d625032b1cc32eb06103ab51
205-22 <33nasrudin@gmail.com>
2025-03-20 15:34:43 +0300

    добавил страницу профиля пользователя

--- scripts/normalizers.js
+++
@@ -1,9 +0,0 @@
-function normalizePhoneNumber(phone) {
-    let normalized = phone.replace(/\D/g, '');
-
-    if (normalized.startsWith('8')) {
-        normalized = '+7' + normalized.slice(1);
-    }
-
-    return normalized;
-}
